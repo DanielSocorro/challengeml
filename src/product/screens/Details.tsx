05971283c101d78d66a8f000f3ce8918799abfc8
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-03-07 19:36:07 -0300

    adding detailpage

--- src/product/screens/Details.tsx
+++ src/product/screens/Details.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Container, Text, Stack, Icon, Box, Image, Input, StackDivider} from "@chakra-ui/react";
 
 import {Product} from "../types";
 import mock from "../mock";
@@ -8,7 +9,16 @@ interface Props {
 }
 
 const DetailsScreen: React.FC<Props> = ({product = mock.product}) => {
-  return <div>{`<DetailsScreen /> para ${product.title}`}</div>;
+  return (
+    <Stack>
+      <Box>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <p>{`Precio: ${product.price} ${product.currency}`}</p>
+        <img alt={product.title} src={product.image} />
+      </Box>
+    </Stack>
+  );
 };
 
 export default DetailsScreen;
