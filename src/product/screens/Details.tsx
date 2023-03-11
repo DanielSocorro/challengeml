a5b67b74527e7d0b8d9a498fe096924e7b455fe8
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-03-11 17:34:19 -0300

    adding details pictures

--- src/product/screens/Details.tsx
+++ src/product/screens/Details.tsx
@@ -16,7 +16,13 @@ const DetailsScreen: React.FC<Props> = ({product = mock.product}) => {
         <p>{product.description}</p>
         <p>${product.base_price}</p>
         <p>${product.price} 22% OFF</p>
-        <img src={product.pictures[0].url} width={200} />
+
+        <Container display="contents" margin={30}>
+          <img src={product.pictures[0].url} width={100} />
+          <img src={product.pictures[1].url} width={100} />
+          <img src={product.pictures[2].url} width={100} />
+          <img src={product.pictures[3].url} width={100} />
+        </Container>
         <p>informacion sobre el vendedor</p>
         <p>{`${product.seller_address.city.name}, ${product.seller_address.state.name}`}</p>
       </Box>
